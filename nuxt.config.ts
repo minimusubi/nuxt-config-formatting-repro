fb1ae3af952a35e7a4fddac3a5aedaacc8ad2bb0
minimusubi <minimusubi@users.noreply.github.com>
2024-07-04 18:43:15 -1000

    npm run dev, then accept suggested compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	future: {
-		compatibilityVersion: 4,
+ future: {
+					compatibilityVersion: 4,
 	},
-	devtools: { enabled: false },
-	// https://nuxt.com/docs/api/nuxt-config#components
+
+ devtools: { enabled: false },
+
+ // https://nuxt.com/docs/api/nuxt-config#components
 	components: [
-		{
-			path: '~/components/global',
-			global: true,
-		},
-		'~/components',
-		// This line is custom
-		// https://github.com/nuxt/nuxt/discussions/17167#discussioncomment-3288741
-		{ path: '~/components/common', pathPrefix: false },
+					{
+									path: '~/components/global',
+									global: true,
+					},
+					'~/components',
+					// This line is custom
+					// https://github.com/nuxt/nuxt/discussions/17167#discussioncomment-3288741
+					{ path: '~/components/common', pathPrefix: false },
 	],
+
+ compatibilityDate: '2024-07-04',
 });
