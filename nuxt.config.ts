e1d42f089e44d42d30a1a0b27ef5c160f69deba2
minimusubi <minimusubi@users.noreply.github.com>
2024-07-04 18:37:36 -1000

    Modify nuxt.config.ts
    There are some path configurations that will cause nuxt to complain, but these
    only exist to further highlight inconsistent indentation at various levels.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	future: {
+		compatibilityVersion: 4,
+	},
+	devtools: { enabled: false },
+	// https://nuxt.com/docs/api/nuxt-config#components
+	components: [
+		{
+			path: '~/components/global',
+			global: true,
+		},
+		'~/components',
+		// This line is custom
+		// https://github.com/nuxt/nuxt/discussions/17167#discussioncomment-3288741
+		{ path: '~/components/common', pathPrefix: false },
+	],
+});
